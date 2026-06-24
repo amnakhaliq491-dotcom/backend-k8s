12296d4246ee7f5d95a4cfcad242ce0fc7cdec25
amnakhaliq491-dotcom <amnakhaliq491@gmail.com>
2026-06-24 14:31:15 +0500

    removing backend and workflow

--- backend/db.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log("Mongo error:", err));
